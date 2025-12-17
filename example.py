330064e9a11983bece9e0ae67cf797832539b976
J.D. Vonck <jvonck@tudelft.nl>
2025-12-18 00:45:14 +0100

    Fix lint errors and make it more uniform

--- example.py
+++ example.py
@@ -1,52 +1,21 @@
 import cProfile
 
-import numpy as np
-
 from sggo.cluster import Cluster
 from sggo.energy import lj
+from sggo.global_opt import basin_hopping
 from sggo.local_opt import bfgs, cg, fire
 from sggo.visualize import plot
-
-
-def hop_basin(cluster, steps, energy, local_opt):
-    cluster_min = local_opt.local_min(cluster)
-    energy_min = energy.energy(cluster_min)
-
-    cluster_current = cluster
-    energy_current = energy_min
-
-    dr = 0.1
-    kT = 100 * 8.617330337217213e-05  # units.kB
-
-    for step in range(steps):
-        ro = cluster_current.positions
-        rn = ro + dr * np.random.uniform(-1.0, 1.0, ro.shape).astype(np.float32)
-        cluster_new = Cluster(rn)
-        cluster_new.ensure_seperation()
-
-        cluster_opt = local_opt.local_min(cluster_new)
-        energy_opt = energy.energy(cluster_opt)
-
-        if energy_opt < energy_min:
-            cluster_min = cluster_opt
-            energy_min = energy_opt
-
-        accept = np.exp((energy_current - energy_opt) / kT) > np.random.uniform()
-        if accept:
-            cluster_current = cluster_new
-            energy_current = energy_opt
-
-        print("basin: ", step, energy_opt, energy_min)
-
-    return energy_min, cluster_min
 
 
 energy = lj.create()
 local_opt = fire.create(energy)
 cluster = Cluster.generate(128)
 
+BH = basin_hopping.BasinHopping(local_opt)
+
 emin, clustermin = None, None
-cProfile.run("emin, clustermin = hop_basin(cluster, 50, energy, local_opt)", sort=1)
+BH.find_minimum(cluster, 2000)
+cProfile.run("emin, clustermin = BH.find_minimum(cluster, 2000)", sort=1)
 
 print("Energy: ", emin)
 plot = plot.ClusterPlot(clustermin)
--- sggo/global_opt/basin_hopping.py
+++ sggo/global_opt/basin_hopping.py
@@ -1,12 +1,9 @@
-from collections import namedtuple
-from numpy.typing import ArrayLike
-from typing import Callable, Optional
-
+from typing import Tuple
 import numpy as np
 
 from sggo.cluster import Cluster
 from sggo.local_opt import LocalOpt
-from sggo.energy.lj import LJCPU
+from sggo.types import NDArray
 
 
 class BasinHopping:
@@ -18,11 +15,11 @@ class BasinHopping:
         rn = ro + dr * np.random.uniform(-1.0, 1.0, ro.shape).astype(np.float32)
         cluster_new = Cluster(rn)
         cluster_new.ensure_seperation()
+
         return cluster_new
 
-    def find_minimum(self, num_atoms: int, num_epochs: int) -> Cluster:
+    def find_minimum(self, cluster_start: Cluster, num_epochs: int) -> Tuple[NDArray, Cluster]:
         energy_fn = self.local_optimizer.energy.energy
-        cluster_start = Cluster.generate(num_atoms)
 
         cluster_min = self.local_optimizer.local_min(cluster_start)
         energy_min = energy_fn(cluster_min)
@@ -32,8 +29,8 @@ class BasinHopping:
 
         dr = 0.1
         kT = 100 * 8.617330337217213e-05  # units.kB
-        
-        for epoch in range(num_epochs):
+
+        for step in range(num_epochs):
             cluster_new = self.mutate(cluster_current, dr)
 
             cluster_opt = self.local_optimizer.local_min(cluster_new)
@@ -48,8 +45,6 @@ class BasinHopping:
                 cluster_current = cluster_new
                 energy_current = energy_opt
 
-            print(epoch)
-        
-        print(energy_min)
+            print("basin: ", step, energy_opt, energy_min)
 
         return energy_min, cluster_min
