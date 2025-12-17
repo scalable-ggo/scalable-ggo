fa3d02b56cbd3730efec5bb4e9f626f8771fd4d6
J.D. Vonck <jvonck@tudelft.nl>
2025-12-18 00:48:24 +0100

    remove obsolete line

--- example.py
+++ example.py
@@ -14,7 +14,6 @@ cluster = Cluster.generate(128)
 BH = basin_hopping.BasinHopping(local_opt)
 
 emin, clustermin = None, None
-BH.find_minimum(cluster, 2000)
 cProfile.run("emin, clustermin = BH.find_minimum(cluster, 2000)", sort=1)
 
 print("Energy: ", emin)
