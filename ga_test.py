fc5eea7e5f240e90d19193a8de89a76541a733e7
Hyonkk <103447476+Hyonkk@users.noreply.github.com>
2026-01-12 20:53:39 +0200

    Ga mutation (#13)
    * Added some basic mutations
    
    * please
    
    * remove old mutation
    
    * added arraylike import
    
    * fix some pipeline errors
    
    * remove redundant lines
    
    * modify clusters with their correct type
    
    * make mutation operators easier to change
    
    * dynamic mutations
    
    * pain
    
    * remove extra import
    
    * fix some linter
    
    * revert to old function
    
    * Fixed merge with main
    
    * Fixed bugs
    
    * Added print statement back
    
    * Fixed skipped prints
    
    ---------
    
    Co-authored-by: Daniel Dumitru <A.D.Dumitru@student.tudelft.nl>
    Co-authored-by: mert yilmaz <mertyilmaz2004@gmail.com>

--- ga_test.py
+++ ga_test.py
@@ -1,58 +1,16 @@
-import matplotlib.pyplot as plt
-import numpy as np
 from mpi4py import MPI
-from numpy.typing import ArrayLike
 
-from sggo.energy.lj import LJCPU
+from sggo.energy import lj
 from sggo.global_opt.genetic import GeneticAlgorithm
-from sggo.local_opt.fire import FIRECPU
+from sggo.local_opt import bfgs, cg, fire
+from sggo.visualize.plot import ClusterPlot
 
+energy = lj.create()
+local_opt = fire.create(energy)
+ga = GeneticAlgorithm(num_candidates=10, local_optimizer=local_opt, operators=[1,2,3,4,5])
 
-def idk(x: ArrayLike) -> ArrayLike:
-    return x
-
-
-def idk2() -> float:
-    return 0.5
-
-
-def main():
-
-    num_candidates = 3
-    num_atoms = 30
-
-    ga = GeneticAlgorithm(
-        num_candidates=num_candidates,
-        local_optimizer=FIRECPU(LJCPU()),
-        mating_distribution=idk2,
-    )
-
-    clusters = [ga.find_minimum(num_atoms, 100)]
-
-    if MPI.COMM_WORLD.rank == 0:
-        fig = plt.figure(figsize=(5 * num_candidates, 5))
-
-        for i, coords in enumerate(map(lambda c: np.asarray(c.positions), clusters)):
-            ax = fig.add_subplot(1, num_candidates, i + 1, projection="3d")
-            ax.scatter(coords[:, 0], coords[:, 1], coords[:, 2])
-
-            ax.set_title(f"Cluster {i + 1}")
-            ax.set_xlabel("x")
-            ax.set_ylabel("y")
-            ax.set_zlabel("z")
-
-            max_range = (coords.max() - coords.min()) / 2.0
-            mid_x = (coords[:, 0].max() + coords[:, 0].min()) / 2.0
-            mid_y = (coords[:, 1].max() + coords[:, 1].min()) / 2.0
-            mid_z = (coords[:, 2].max() + coords[:, 2].min()) / 2.0
-
-            ax.set_xlim(mid_x - max_range, mid_x + max_range)
-            ax.set_ylim(mid_y - max_range, mid_y + max_range)
-            ax.set_zlim(mid_z - max_range, mid_z + max_range)
-
-        plt.tight_layout()
-        plt.show()
-
-
-if __name__ == "__main__":
-    main()
+energymin, clustermin = ga.find_minimum(128, 50)
+if MPI.COMM_WORLD.rank == 0:
+    print("Energy: ", energymin)
+    plot = ClusterPlot(clustermin)
+    plot.plot()
--- sggo/global_opt/genetic.py
+++ sggo/global_opt/genetic.py
@@ -1,10 +1,7 @@
 from enum import IntEnum
-from typing import Callable
-
 import numpy as np
 from mpi4py import MPI
 from numpy.typing import ArrayLike
-
 from sggo.cluster import Cluster
 from sggo.local_opt import LocalOpt
 
@@ -14,28 +11,34 @@ class GAMPITag(IntEnum):
     TAG_EXIT = 2
 
 
+class MutationOperator(IntEnum):
+    ANGULAR = 0
+    CARTESIAN = 1
+    DYNAMIC = 2
+    GEOMETRIC_CENTER = 3
+    INTERIOR = 4
+    RADIAL_SWAP = 5
+
+
 class GeneticAlgorithm:
-    def __init__(self, num_candidates: int, local_optimizer: LocalOpt, mating_distribution: Callable[[], float]):
+
+    def __init__(self, num_candidates: int, local_optimizer: LocalOpt, operators: list[int | MutationOperator] | None = None):
         self.num_candidates = num_candidates
         self.local_optimizer = local_optimizer
-        self.mating_distribution = mating_distribution
+        self.operators = operators
 
-    def boltzmann_weights(self, energies):
-        e = np.array(energies)
-        emin = e.min()
-        betaE = (e - emin)
-        w = np.exp(-betaE)
-        w /= w.sum()
-        return w
-
-    def mutate(self, cluster: ArrayLike) -> ArrayLike:
+    def mutate(self, cluster: Cluster) -> Cluster:
+        choice: int
         rng = np.random.default_rng()
-        choice: int = rng.integers(0, 5)
-        N: int = cluster.shape[0]
+        if self.operators is not None and len(self.operators) != 0:
+            choice = rng.choice([int(op) for op in self.operators])
+        else:
+            return cluster
+        N: int = cluster.positions.shape[0]
 
         # the distance to the nearest neighbour of the ith atom
         def nn_dist(i: int) -> float:
-            diff: ArrayLike = cluster - cluster[i]
+            diff: ArrayLike = cluster.positions - cluster.positions[i]
             dists: ArrayLike = np.linalg.norm(diff, axis=1)
             dists[i] = np.inf
             return np.min(dists)
@@ -45,12 +48,12 @@ class GeneticAlgorithm:
             case 0:
                 noAtoms: int = rng.integers(1, max(2, int(N / 20) + 1)) # 1-5%, otherwise take 1 as default
                 chosenAtoms: ArrayLike = rng.choice(N, size=noAtoms, replace=False)
-                center: ArrayLike = np.mean(cluster, axis=0)
+                center: ArrayLike = np.mean(cluster.positions, axis=0)
                 for i in chosenAtoms:
-                    radius: float = np.linalg.norm(cluster[i] - center)
+                    radius: float = np.linalg.norm(cluster.positions[i] - center)
                     direction: ArrayLike = rng.normal(size=3)
                     direction /= np.linalg.norm(direction)
-                    cluster[i] = center + radius * direction
+                    cluster.positions[i] = center + radius * direction
             # Cartesian Displacement Operator
             case 1:
                 S: float = 0.2
@@ -59,39 +62,61 @@ class GeneticAlgorithm:
                 for i in chosenAtoms:
                     rmin: float = nn_dist(i)
                     displacement: ArrayLike = rng.uniform(-1.0, 1.0, size=3)
-                    cluster[i] += (S * rmin) * displacement
+                    cluster.positions[i] += (S * rmin) * displacement
             # Dynamic Mutation
             case 2:
                 gamma: float = 0.10
-                cluster *= rng.uniform(1.0 - gamma, 1.0 + gamma, size=(N, 3))
+                cluster.positions *= rng.uniform(1.0 - gamma, 1.0 + gamma)
             # Geometric Center Displacement Operator
             case 3:
                 amax: float = 0.2
                 amin: float = 0.7
                 w: float = 2.0
 
-                center: ArrayLike = np.mean(cluster, axis=0)
-                rMax: float = np.max(np.linalg.norm(cluster - center, axis=1))
+                center: ArrayLike = np.mean(cluster.positions, axis=0)
+                rMax: float = np.max(np.linalg.norm(cluster.positions - center, axis=1))
 
                 noAtoms: int = rng.integers(1, N + 1)
                 chosenAtoms: ArrayLike = rng.choice(N, size=noAtoms, replace=False)
 
                 for i in chosenAtoms:
-                    ri: float = np.linalg.norm(cluster[i] - center)
+                    ri: float = np.linalg.norm(cluster.positions[i] - center)
                     rmin: float = nn_dist(i)
                     direction: ArrayLike = rng.normal(size=3)
                     direction /= np.linalg.norm(direction)
-                    cluster[i] += ((amax - amin) * (ri / rMax)**w + amin) * rmin * direction
+                    cluster.positions[i] += ((amax - amin) * (ri / rMax)**w + amin) * rmin * direction
             # Interior Operator
             case 4:
                 atom_index: int = rng.integers(0, N)
-                center: ArrayLike = np.mean(cluster, axis=0)
-                ri: float = np.linalg.norm(cluster[atom_index] - center)
+                center: ArrayLike = np.mean(cluster.positions, axis=0)
+                ri: float = np.linalg.norm(cluster.positions[atom_index] - center)
                 radius: float = rng.uniform(0.01, 0.10) * ri
                 direction: ArrayLike = rng.normal(size=3)
                 direction /= np.linalg.norm(direction)
-                cluster[atom_index] = center + radius * direction
+                cluster.positions[atom_index] = center + radius * direction
+            # Radial shell swap
+            case 5:
+                center = cluster.positions.mean(axis=0)
+                rel = cluster.positions - center
+                r = np.linalg.norm(rel, axis=1)
+                idx = np.argsort(r)
+                n_core = max(1, N // 4)
+                n_surf = max(1, N // 4)
 
+                core_atoms = idx[:n_core]
+                surf_atoms = idx[-n_surf:]
+
+                i = rng.choice(core_atoms)
+                j = rng.choice(surf_atoms)
+
+                ui = rel[i] / (r[i] + 1e-12)
+                uj = rel[j] / (r[j] + 1e-12)
+
+                ri = r[j] * rng.uniform(0.9, 1.1)
+                rj = r[i] * rng.uniform(0.9, 1.1)
+
+                cluster.positions[i] = center + ri * ui
+                cluster.positions[j] = center + rj * uj
         return cluster
 
     def mate(self, cluster1: Cluster, cluster2: Cluster) -> Cluster:
@@ -122,9 +147,20 @@ class GeneticAlgorithm:
                 i -= 1
             else:
                 j += 1
-        return Cluster(np.concat([p1[idx1[:i]], p2[idx2[i:]]]))
+        return Cluster(np.concatenate([p1[idx1[:i]], p2[idx2[i:]]]))
 
-    def find_minimum(self, num_atoms: int, num_epochs: int, mutation_rate: float = 0.05, energy_resolution: float = 1e-3) -> Cluster:
+    def boltzmann_choice(self, rng, temp, energies, clusters):
+        e = np.asarray(energies, dtype=float).ravel()
+        emin = e.min()
+        betaE = (e - emin)
+        w = np.exp(-betaE / temp)
+        w /= w.sum()
+
+        inds = rng.choice(len(energies), p=w, size=2, replace=False)
+        return clusters[inds[0]], clusters[inds[1]]
+
+    def find_minimum(self, num_atoms: int, num_epochs: int, mutation_rate: float = 0.15,
+                      energy_resolution: float = 1e-3, target: float | None = None) -> tuple[Cluster, float]:
         comm = MPI.COMM_WORLD
         rank = comm.rank
         size = comm.size
@@ -132,72 +168,90 @@ class GeneticAlgorithm:
         rng = np.random.default_rng()
         energy_fn = lambda cluster: np.squeeze(self.local_optimizer.energy.energy(cluster))
 
+        def create_child(parent1, parent2, mutation_rate):
+            child = self.mate(parent1, parent2)
+
+            if rng.random() < mutation_rate:
+                child = self.mutate(child)
+
+            child.ensure_seperation()
+
+            child_relaxed = self.local_optimizer.local_min(child)
+            child_energy = energy_fn(child_relaxed)
+
+            return child_energy, child_relaxed
+
+        def create_immigrant():
+            immigrant = Cluster.generate(num_atoms)
+            immigrant_relaxed = self.local_optimizer.local_min(immigrant)
+            immigrant_energy = energy_fn(immigrant_relaxed)
+
+            return immigrant_energy, immigrant_relaxed
+
         if rank == 0:
             # controller process
-            clusters = [Cluster.generate(num_atoms) for _ in range(self.num_candidates)]
+            clusters = []
             energies = []
 
-            for i, cl in enumerate(clusters):
-                relaxed = self.local_optimizer.local_min(cl)
-                E = energy_fn(relaxed)
-                energies.append(E)
-                clusters[i] = relaxed
+            for _ in range(self.num_candidates):
+                energy, cluster = create_immigrant()
+
+                clusters.append(cluster)
+                energies.append(energy)
 
             best_idx = int(np.argmin(energies))
             best_cluster = clusters[best_idx].copy()
             best_energy = energies[best_idx]
 
+            T0 = 1.0
+            Tmin = 1e-3
+
+            last_improvement = 0
+            IMMIGRATION_DELAY = 10 * max(1, self.num_candidates // 5)
+            IMMIGRATION_FRACTION = 0.1
+
             # give all workers an initial task to do
             for i in range(1, size):
-                weights = self.boltzmann_weights(energies)
-                i1 = rng.choice(len(clusters), p=weights)
-                i2 = rng.choice(len(clusters), p=weights)
-                while i2 == i1:
-                    i2 = rng.choice(len(clusters), p=weights)
-
-                parent1 = clusters[i1]
-                parent2 = clusters[i2]
+                parent1, parent2 = self.boltzmann_choice(rng, T0, energies, clusters)
                 comm.Send([np.append(parent1.positions.flatten(), parent2.positions.flatten()), MPI.FLOAT], dest=i, tag=GAMPITag.TAG_MSG)
 
-            epochs = size - 1
+            workers = size - 1
+            matings = num_epochs - workers
 
-            for _i in range(num_epochs):
-                weights = self.boltzmann_weights(energies)
-                i1 = rng.choice(len(clusters), p=weights)
-                i2 = rng.choice(len(clusters), p=weights)
-                while i2 == i1:
-                    i2 = rng.choice(len(clusters), p=weights)
+            for curr_epoch in range(num_epochs):
+                if matings == 0 and workers == 0:
+                    break
+                print(f"Epoch {curr_epoch} | " f"best E={best_energy}")
 
-                parent1 = clusters[i1]
-                parent2 = clusters[i2]
+                T = max(Tmin, T0 * np.exp(-curr_epoch / (0.3 * num_epochs)))
+                parent1, parent2 = self.boltzmann_choice(rng, T, energies, clusters)
 
                 child_relaxed = None
                 child_energy = None
 
+                p_mut = np.float32(mutation_rate * np.exp(-curr_epoch / num_epochs))
+
                 if size == 1:
                     # if there is no other processes create a child in the main process
-                    child = self.mate(parent1, parent2)
-
-                    if rng.random() < mutation_rate:
-                        child = self.mutate(child)
-
-                    child.ensure_seperation()
-
-                    child_relaxed = self.local_optimizer.local_min(child)
-                    child_energy = energy_fn(child_relaxed)
+                    child_energy, child_relaxed = create_child(parent1, parent2, p_mut)
                 else:
                     data = np.zeros(3 * num_atoms + 1, dtype=np.float32)
                     status = MPI.Status()
 
                     comm.Recv(data, source=MPI.ANY_SOURCE, tag=GAMPITag.TAG_MSG, status=status) # recieve any children finished
-                    if epochs < num_epochs:
+                    if matings > 0:
                         # assign the next mating task to the finished process
-                        comm.Send([np.append(parent1.positions.flatten(), parent2.positions.flatten()), MPI.FLOAT],
-                                  dest=status.Get_source(), tag=GAMPITag.TAG_MSG)
-                        epochs += 1
+                        send_data = np.concatenate((
+                            np.array([p_mut]),
+                            parent1.positions.flatten(),
+                            parent2.positions.flatten()
+                        ))
+                        comm.Send([send_data, MPI.FLOAT], dest=status.Get_source(), tag=GAMPITag.TAG_MSG)
+                        matings -= 1
                     else:
                         # ask the process to exit as the desired number of epochs was reached
                         comm.Send([np.zeros(0), MPI.FLOAT], dest=status.Get_source(), tag=GAMPITag.TAG_EXIT)
+                        workers -= 1
 
                     child_relaxed = Cluster(data[1:].reshape(-1, 3))
                     child_energy = data[0]
@@ -207,22 +261,40 @@ class GeneticAlgorithm:
                     if abs(E - child_energy) < energy_resolution:
                         duplicate = True
                         break
-                if duplicate:
+                if duplicate and rng.random() < 0.9:
                     continue
 
                 worst_idx = int(np.argmax(energies))
-                if child_energy < energies[worst_idx]:
+                dE = child_energy - energies[worst_idx]
+
+                if dE < 0 or rng.random() < np.exp(-dE / T):
                     clusters[worst_idx] = child_relaxed
                     energies[worst_idx] = child_energy
 
                     if child_energy < best_energy:
                         best_energy = child_energy
                         best_cluster = child_relaxed.copy()
+                        last_improvement = curr_epoch
 
-            return best_cluster
+                if curr_epoch - last_improvement >= IMMIGRATION_DELAY:
+                    num_immigrants = max(1, int(self.num_candidates * IMMIGRATION_FRACTION))
+                    worst_indices = np.argsort(energies)[-num_immigrants:]
+
+                    for idx in worst_indices:
+                        energy, cluster = create_immigrant()
+                        clusters[idx] = cluster
+                        energies[idx] = energy
+
+                    last_improvement = curr_epoch
+
+                if target is not None and best_energy <= target and matings > 0:
+                    print("Target reached in", curr_epoch, "epochs")
+                    matings = 0
+
+            return best_energy, best_cluster
         else:
             # worker process
-            data = np.zeros(2 * 3 * num_atoms, dtype=np.float32)
+            data = np.zeros(2 * 3 * num_atoms + 1, dtype=np.float32)
             status = MPI.Status()
 
             while True:
@@ -231,17 +303,10 @@ class GeneticAlgorithm:
                 if status.Get_tag() == GAMPITag.TAG_EXIT:
                     break
 
-                parent1 = Cluster(data[0:3 * num_atoms].reshape(-1, 3))
-                parent2 = Cluster(data[3 * num_atoms:].reshape(-1, 3))
+                p_mut = data[0]
+                parent1 = Cluster(data[1:3 * num_atoms + 1].reshape(-1, 3))
+                parent2 = Cluster(data[3 * num_atoms + 1:].reshape(-1, 3))
 
-                child = self.mate(parent1, parent2)
-
-                if rng.random() < mutation_rate:
-                    child = self.mutate(child)
-
-                child.ensure_seperation()
-
-                child_relaxed = self.local_optimizer.local_min(child)
-                child_energy = energy_fn(child_relaxed)
-
+                child_energy, child_relaxed = create_child(parent1, parent2, p_mut)
                 comm.Send([np.append(child_energy, child_relaxed.positions.flatten()), MPI.FLOAT], dest=0, tag=GAMPITag.TAG_MSG)
+            return None, None
